6215f2b25f5fa1c71260fa9d22c1824dd236f8c3
username <bevcook126@gmail.com>
2022-10-03 13:58:07 -0700

    resume cards

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -6,7 +6,7 @@ export default function Resume() {
         <h1 id="resume">RESUME</h1>
         <div className="cards">
             <div className="resume-card">
-                <h2>Software Engineering Fellow - General Assembly, Remote</h2>
+                <h3>Software Engineering Fellow - General Assembly, Remote</h3>
                 <ul>
                     <li>Created full MERN Stack applications</li>
                     <li>Implemented full CRUD</li>
@@ -14,7 +14,7 @@ export default function Resume() {
                 </ul>
             </div>
             <div className="resume-card">
-              <h2>K/TK Teacher - Kids N' Dance N' Theater Arts, Oakland CA</h2>
+              <h3>K/TK Teacher - Kids N' Dance N' Theater Arts, Oakland CA</h3>
                 <ul>
                     <li>Worked alongside Director to create program from the ground up.</li>
                     <li>Monitored students' academic, social, and emotional progress and recorded individual files.</li>
@@ -22,7 +22,7 @@ export default function Resume() {
                 </ul>
             </div>
             <div className="resume-card">
-                <h2>K/1 Teacher - Rocketship Public Schools, Concord CA</h2>
+                <h3>K/1 Teacher - Rocketship Public Schools, Concord CA</h3>
                 <ul>
                     <li>Delivered lesson plans, implemented school curriculum, and used data to create, monitor, and establish personalized learning plan for each student.</li>
                     <li>Supported students in achieving highest Math growth in school.</li>
@@ -30,7 +30,7 @@ export default function Resume() {
                 </ul>                
             </div>
             <div className="resume-card">
-                <h2>Teacher/Site Supervisor - Child Development Resource Center, San Luis Obispo CA</h2>
+                <h3>Teacher/Site Supervisor - Child Development Resource Center, San Luis Obispo CA</h3>
                 <ul>
                     <li>Consulted with Director and site's Child Therapist to drive program efficiency.</li>
                     <li>Observed children to identify individuals in need of additional support and developed strategies to improve assistance.</li>
