68a6e0e2e6122b09ac5206b9bac4e59ed7943050
username <bevcook126@gmail.com>
2022-10-04 21:23:57 -0700

    initial doodle theme change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <>
         <h1>Beverly Cook</h1>
         <div className="aboutme">
-            <img src="https://i.imgur.com/mIgUVUw.png" alt="Beverly Cook headshot"/>
+            <img src="https://i.imgur.com/hDQlc2f.png" alt="Beverly Cook headshot"/>
             <p>I am a software engineer with a passion for joy and learning. These interests sparked during my teaching career. While in this role, I discovered that joy is a necessary ingredient for life, learning, and success. While teaching Kindergarteners and coaching other teachers, I was able to lead with empathy and resolve conflict both in and out of the classroom. When the pandemic first hit, I adapted quickly to distance learning and was frequently used as a resource to help my students as well as other teachers access technology and programs that were new to them. I’m looking to launch a career in Education Technology in a Software Engineering role. I hope to work with an innovative team working towards making the use of technology an accessible and joyful experience for teachers, students, and the average user.</p>
         </div>
         </>
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -5,7 +5,7 @@ export default function Resume() {
         <>
         <h1 id="resume">RESUME</h1>
         <div className="cards">
-            <div className="resume-card">
+            <div className="resume-card" id="ga">
                 <h3>Software Engineering Fellow - General Assembly, Remote</h3>
                 <ul>
                     <li>Created full MERN Stack applications</li>
@@ -13,7 +13,7 @@ export default function Resume() {
                     <li>Gained proficiency in JavaScript, Python, CSS, and HTML</li>
                 </ul>
             </div>
-            <div className="resume-card">
+            <div className="resume-card" id="knd">
               <h3>K/TK Teacher - Kids N' Dance N' Theater Arts, Oakland CA</h3>
                 <ul>
                     <li>Worked alongside Director to create program from the ground up.</li>
@@ -21,7 +21,7 @@ export default function Resume() {
                     <li>Identified characteristics, needs, and abilities of each child through personal interaction and progress reports to optimize approaches.</li>
                 </ul>
             </div>
-            <div className="resume-card">
+            <div className="resume-card" id="rfa">
                 <h3>K/1 Teacher - Rocketship Public Schools, Concord CA</h3>
                 <ul>
                     <li>Delivered lesson plans, implemented school curriculum, and used data to create, monitor, and establish personalized learning plan for each student.</li>
@@ -29,7 +29,7 @@ export default function Resume() {
                     <li>As grade level lead, mentored other teachers to use various EdTech tools such as Nearpod, Peardeck, Quizizz, and GoGuardian.</li>
                 </ul>                
             </div>
-            <div className="resume-card">
+            <div className="resume-card" id="cdrc">
                 <h3>Teacher/Site Supervisor - Child Development Resource Center, San Luis Obispo CA</h3>
                 <ul>
                     <li>Consulted with Director and site's Child Therapist to drive program efficiency.</li>
